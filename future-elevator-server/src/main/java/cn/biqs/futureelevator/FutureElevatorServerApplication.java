caf5f3dcbf8cc6e053642f5ac3a679e2deccbc0f
Liangdi <wu@liangdi.me>
2018-11-10 14:21:21 +0800

    init spring project

---
+++ future-elevator-server/src/main/java/cn/biqs/futureelevator/FutureElevatorServerApplication.java
@@ -0,0 +1,12 @@
+package cn.biqs.futureelevator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureElevatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureElevatorServerApplication.class, args);
+	}
+}
