5dbe919df8b270e9a6a33ef1b31632c3d583159b
Liangdi <wu@liangdi.me>
2018-11-10 14:23:18 +0800

    init spring project

--- future-elevator-server/src/test/java/cn/biqs/futureelevator/FutureElevatorServerApplicationTests.java
+++ future-elevator-server/src/test/java/cn/biqs/futureelevator/FutureElevatorServerApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FutureElevatorServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
